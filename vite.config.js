56f14105b0d6b00959402e81b30121fe02903b6b
weswai <waiting_lam@hotmail.com>
2023-08-07 16:05:20 -0700

    Add dist

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/vue-todo/'
+  : '/',
   plugins: [vue()],
 })
